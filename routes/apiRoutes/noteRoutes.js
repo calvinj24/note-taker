64a17c1f08a3e868bf25c5c5f0a975d5d0fc5f5f
Calvin Johnson <calvinjohnson@Calvins-MBP-7.attlocal.net>
2021-01-05 14:54:59 -0600

    add API Routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,25 +1,29 @@
 const router = require("express").Router();
-const { createNewNote } = require('../../lib/notes')
+const { findById, createNewNote } = require('../../lib/notes')
 const { notes } = require("../../db");
+const uniqid = require('uniqid');
 
 // get all notes
 router.get("/notes", (req, res) => {
   let results = notes;
-
   res.json(results);
 });
 
 // get notes by id
-//router.get("/notes/:id", (req, res) => {
-//  let results = data;
-//
-//  res.json(results);
-//});
+router.get("/notes/:id", (req, res) => {
+  console.log(req.params.id);
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(400)
+  }
+});
 
 // add new note
 router.post('/notes', (req, res) => {
+  req.body.id = uniqid();
   //console.log(req.body);
-  req.body.id = 1;
   const note = createNewNote (req.body, notes);
   res.json(note);
 });
